fb7be551a252cc865f6eb9f4ccc18982aa613e10
wwqgtxx <wwqgtxx@gmail.com>
2014-02-09 11:53:55 +0800

    bat

--- push.bat
+++ push.bat
@@ -1,8 +1,15 @@
 git pull
 pause
+sort list.txt | uniq > list2.txt
+rm list.txt
+cat list2.txt>list.txt
+rm list2.txt
+cat head.txt list.txt > whitelist.txt
+cat whitelist.txt
+pause
 openssl base64 -e -in whitelist.txt -out whitelist-base64.txt
 git config --global push.default matching
-git config core.autocrlf false
+git config core.autocrlf true
 git init
 git add --all .
 git add -u
